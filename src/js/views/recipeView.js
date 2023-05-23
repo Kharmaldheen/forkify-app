74acf050a1073870d8d3f2fb032c3dfd072bffc3
kharmaldheen <abdullahikamaldeen3@gmail.com>
2023-05-23 09:49:02 -0700

    new

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -158,7 +158,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ingredient.quantity ? this.decimalToFraction(ingredient.quantity) : ''
+      ingredient.quantity ? this.decimalToFraction(+ingredient.quantity) : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ingredient.unit}</span>
