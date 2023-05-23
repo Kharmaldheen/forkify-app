73feb22985eab61bc90b8af735afe0db8733d68f
kharmaldheen <abdullahikamaldeen3@gmail.com>
2023-05-23 09:55:00 -0700

    testing

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -29,6 +29,7 @@ class RecipeView extends View {
     // Format the fraction as a string
     let fractionString =
       numerator === denominator ? '1' : numerator + '/' + denominator;
+
     return fractionString;
   }
 
@@ -158,7 +159,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ingredient.quantity ? this.decimalToFraction(+ingredient.quantity) : ''
+      ingredient.quantity ? this.decimalToFraction(0.43) : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ingredient.unit}</span>
