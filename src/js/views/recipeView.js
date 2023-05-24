622b54f2f56724c235bdd061fc1c223515a6514e
kharmaldheen <abdullahikamaldeen3@gmail.com>
2023-05-24 03:48:19 -0700

    fixed

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -138,7 +138,7 @@ class RecipeView extends View {
       <use href="${icons}#icon-check"></use>
     </svg>
     <div class="recipe__quantity">${
-      ingredient.quantity ? new Fraction(ingredient.quantity).toString() : ''
+      ingredient.quantity ? Fraction(ingredient.quantity).toString() : ''
     }</div>
     <div class="recipe__description">
       <span class="recipe__unit">${ingredient.unit}</span>
