5769d069b2cefb49f8150a46b1356449775f4e63
kharmaldheen <abdullahikamaldeen3@gmail.com>
2023-05-26 13:12:41 -0700

    fixed error

--- src/js/config.js
+++ src/js/config.js
@@ -7,7 +7,7 @@ const apiKey2 = process.env.API_KEY2;
 export default {
   apiKey1,
   apiKey2,
-}
+};
 
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
@@ -16,5 +16,3 @@ export const TIMEOUT_SEC = 10;
 export const RESULT_PER_PAGE = 10;
 
 export const MODAL_CLOSE_SEC = 2.5;
-
-export default config;
