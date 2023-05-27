c90b1c30cd0f1898894c053720a0189b878984b8
kharmaldheen <abdullahikamaldeen3@gmail.com>
2023-05-27 13:22:02 -0700

    new

--- src/js/config.js
+++ src/js/config.js
@@ -2,9 +2,9 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 
 export const TIMEOUT_SEC = 10;
 
-API_KEY1 = '57242800-24e4-4e73-a3c8-9968110d38ef';
+export const API_KEY1 = '57242800-24e4-4e73-a3c8-9968110d38ef';
 
-API_KEY2 = 'c77b5a57e48d400bb170f6cdae246c69';
+export const API_KEY2 = 'c77b5a57e48d400bb170f6cdae246c69';
 
 export const RESULT_PER_PAGE = 10;
 
